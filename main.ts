f25c2531168c03ab4752425417f2ab000c04e998
alardizabalsha <113388146+alardizabalsha@users.noreply.github.com>
2022-11-01 14:00:39 -0700

    singleplayer fixing

--- main.ts
+++ main.ts
@@ -11,6 +11,12 @@ namespace SpriteKind {
  * 
  * 2. Fix bugs: Players can attack the same location twice, boat placement bugs
  */
+function cpumove () {
+    game.splash("CPU Move")
+    grid.place(cursor, tiles.getTileLocation(randint(0, 9), randint(0, 6)))
+    isHitOrMiss(boatSpriteArrayP1, hitOrMissP2)
+    switchPlayer()
+}
 // moveBoat needs changes to take in the boatRotateArrayP1 or boatRotateArrayP2
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     rotateFlag = "nothing"
@@ -95,11 +101,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         if (currentPlayer == "Player1") {
             isHitOrMiss(boatSpriteArrayP2, hitOrMissP1)
             switchPlayer()
-        } else if (singlePlayerFlag == 1) {
-            game.splash("CPU Move")
-            grid.place(cursor, tiles.getTileLocation(randint(0, 9), randint(0, 6)))
-            isHitOrMiss(boatSpriteArrayP1, hitOrMissP2)
-            switchPlayer()
         } else {
             isHitOrMiss(boatSpriteArrayP1, hitOrMissP2)
             switchPlayer()
@@ -133,6 +134,9 @@ function switchPlayer () {
         }
         makeBoatInvisible(hitOrMissP1)
         makeBoatVisible(hitOrMissP2)
+        if (singlePlayerFlag == 1 && moveBoatFlag == 3) {
+            cpumove()
+        }
     } else {
         currentPlayer = "Player1"
         for (let boatIterator of boatSpriteArrayP2) {
@@ -143,7 +147,6 @@ function switchPlayer () {
     }
 }
 function cpuPlaceBoat0 () {
-    makeBoatVisible(boatSpriteArrayP2[0])
     if (randint(0, 1) == 0) {
         grid.place(cursor, tiles.getTileLocation(randint(0, 8), randint(0, 6)))
         grid.place(boatSpriteArrayP2[0][0], grid.add(grid.getLocation(cursor), 0, 0))
@@ -187,6 +190,13 @@ function moveBoat (boatArray: any[], boatRotateArray: string[]) {
     }
 }
 function isHitOrMiss (enemyBoats: Sprite[][], hitOrMissPX: Sprite[]) {
+    if (currentPlayer == " Player1") {
+        hitormissplayer = "Player 1"
+    } else if (currentPlayer == " Player2" && singlePlayerFlag == 1) {
+        hitormissplayer = "you"
+    } else {
+        hitormissplayer = "Player 2"
+    }
     for (let index = 0; index <= 2; index++) {
         for (let currentBoatSprite of enemyBoats[index]) {
             if (grid.spriteCol(currentBoatSprite) == grid.spriteCol(cursor) && grid.spriteRow(currentBoatSprite) == grid.spriteRow(cursor)) {
@@ -210,7 +220,7 @@ function isHitOrMiss (enemyBoats: Sprite[][], hitOrMissPX: Sprite[]) {
                     `, SpriteKind.Projectile)
                 grid.place(boomSprite, grid.getLocation(cursor))
                 hitOrMissPX.push(boomSprite)
-                game.splash("" + currentPlayer + " HIT!! " + convertToText(isPlayerXWinner(enemyBoats, hitOrMissPX)) + " boats destroyed!")
+                game.splash("" + hitormissplayer + " HIT!! " + convertToText(isPlayerXWinner(enemyBoats, hitOrMissPX)) + " boats destroyed!")
                 return 1
             }
         }
@@ -235,7 +245,7 @@ function isHitOrMiss (enemyBoats: Sprite[][], hitOrMissPX: Sprite[]) {
         `, SpriteKind.Projectile)
     grid.place(boomSprite, grid.getLocation(cursor))
     hitOrMissPX.push(boomSprite)
-    game.splash("" + currentPlayer + " MISS!!")
+    game.splash("" + hitormissplayer + " MISS!!")
     return 0
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -427,7 +437,6 @@ function initP1 () {
         `, SpriteKind.Projectile)]
 }
 function cpuPlaceBoat1 () {
-    makeBoatVisible(boatSpriteArrayP2[1])
     if (randint(0, 1) == 0) {
         grid.place(cursor, tiles.getTileLocation(randint(0, 7), randint(0, 6)))
         grid.place(boatSpriteArrayP2[1][0], grid.add(grid.getLocation(cursor), 0, 0))
@@ -629,7 +638,6 @@ function makeBoatInvisible (boatArray: Sprite[]) {
     }
 }
 function cpuPlaceBoat2 () {
-    makeBoatVisible(boatSpriteArrayP2[2])
     if (randint(0, 1) == 0) {
         grid.place(cursor, tiles.getTileLocation(randint(0, 6), randint(0, 6)))
         grid.place(boatSpriteArrayP2[2][0], grid.add(grid.getLocation(cursor), 0, 0))
@@ -664,14 +672,15 @@ function isOverlapping (boatSpriteArrayPX: Sprite[][]) {
     return 0
 }
 let boomSprite: Sprite = null
+let hitormissplayer = ""
 let iterator = 0
-let hitOrMissP2: Sprite[] = []
 let hitOrMissP1: Sprite[] = []
 let currentBoatBoomCounter = 0
 let killCount = 0
 let boatRotateArrayP2: string[] = []
 let boatSpriteArrayP2: Sprite[][] = []
 let boatRotateArrayP1: string[] = []
+let hitOrMissP2: Sprite[] = []
 let boatSpriteArrayP1: Sprite[][] = []
 let shadowCursor: Sprite = null
 let cursor: Sprite = null
@@ -681,7 +690,11 @@ let rotateFlag = ""
 let currentPlayer = ""
 let singlePlayerFlag = 0
 tiles.setCurrentTilemap(tilemap`level1`)
-singlePlayerFlag = 1
+if (game.ask(" Singleplayer", " Multiplayer")) {
+    singlePlayerFlag = 1
+} else {
+    singlePlayerFlag = 0
+}
 currentPlayer = "Player1"
 initP1()
 initP2()
